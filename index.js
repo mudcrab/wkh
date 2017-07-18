ad60d56653c7018475319fc5d1848ab4fdac6d36
Jevgeni Krasnosjolov <jevgeni@pitfire.io>
2017-07-18 10:03:42 +0300

    add exports to index.js, remove testing code

---
+++ index.js
@@ -0,0 +1,7 @@
+const Wkh = require('./dist/wkh');
+const WkhResource = require('./dist/wkh_resource');
+
+module.exports = {
+	Wkh: Wkh,
+	WkhResource: WkhResource
+};
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,4 @@
 import { Wkh } from './wkh';
 import { WkhResource, WkhFunctionResource } from './wkh_resource';
 
-
-
-class Test extends WkhResource
-{
-	constructor()
-	{
-		super();
-	}
-	kek()
-	{
-		return new Promise((resolve) => {
-			resolve({ a: true })
-		})
-	}
-
-	static prefix()
-	{
-		return '';
-	}
-
-	static routes( prefix:string ): WkhFunctionResource[]
-	{
-		return [
-			{
-				type: 'get',
-				method: 'kek',
-				path: '/kek'
-			}
-		];
-	}
-}
-
-(new Wkh({}))
-	.mountRoutes([ Test, { type: 'get', fn: () => {
-		return new Promise((r) => { r({ top: 'top'})});
-	}, path: '/top' } ])
-	.run(3000, () => { console.log('running')});
-
-
-export { Wkh };
+export { Wkh, WkhResource, WkhFunctionResource };
